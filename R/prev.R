ead22e60171cd6e6ba8793c80b793b8fcbe690f2
solarmouse <esolarflare@gmail.com>
2020-07-30 16:26:18 -0400

    Trying to fix the prev function. Still not working, but now uses a for loop

--- R/prev.R
+++ R/prev.R
@@ -8,12 +8,13 @@ library(tidyverse)
 prev.mod <- function(x,y){
   df <- as.data.frame(x)
   prev <- y
-
-  new_df <- df %>%
-    arrange(Lake_Name, Round)%>%
-    group_by(Lake_Name, Round)%>%
-    mutate(overall_m_prev = overall_m_prev > y)%>%
-    distinct() %>% as.data.frame()
-
-  return(new_df)
+  for (i in 1:length(levels(df$Lake_Name))) {
+    df$max_prev[i]<- mean(df$overal_m_prev[which(df$Lake_Name==levels(df$Lake_Name)[i])], na.rm = TRUE) >0
+    'selected_prev <- df$max_prev[which(df$Lake_Name==levels(df$Lake_Name)[i])] > prev'}
+  'new_df <- subset(df, df$selected_prev == TRUE)'
+  return(df)
 }
+
+'Create for loop that cycles through lakes use which command and calc max value
+then make another column that is TRUE of FALSE, then subset data if ==TRUE, return
+new dataframe'
